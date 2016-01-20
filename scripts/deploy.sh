4a913933c123e608eb9d9363e9be401e61feb338
ahmet kizilay <ahmet.kizilay@gmail.com>
2016-01-20 21:55:02 +0200

    simplified deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,6 @@ pm2 stop start-with-newrelic
 git fetch origin
 git merge origin/master
 
-if git diff --name-only HEAD@{1} HEAD package.json | grep -e 'package.json'; then
-  npm install
-fi
+npm install
 
 pm2 start start-with-newrelic
